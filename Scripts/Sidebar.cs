82adee96b9356cb5226b8fdb577aabbe8057dedf
Raven4433 <100307827+Raven4433@users.noreply.github.com>
2025-12-29 11:06:04 +0100

    Create Sidebar.cs

---
+++ Scripts/Sidebar.cs
@@ -0,0 +1,47 @@
+
+using UnityEngine;
+using Studio.Tools;
+
+
+namespace Studio {
+
+public class Sidebar : MonoBehaviour {
+    
+
+
+
+
+
+    void Awake(){
+        
+    }
+
+
+
+    void Start(){
+
+        Core.hotkeys.Register("OpenCloseSidebar", KeyCode.F1, Modifier.None, OpenCloseSidebar);
+
+    }
+
+
+
+
+    void OpenCloseSidebar(){
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+
+
+
+
+
+    void OnDestroy(){
+        // Always unregister to prevent memory leaks or ghost calls
+        if(Core.hotkeys != null){ Core.hotkeys.Unregister("OpenCloseSidebar"); }
+    }
+
+
+
+
+}
+}
